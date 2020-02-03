d1ba1381f8cb6da9927a07908483cdbfbb5b6e2d
natam369 <58681656+natam369@users.noreply.github.com>
2020-02-03 11:39:34 +0530

    Update local_exec.tf

--- 04-Terraform/03-Ansible-Deploy/local_exec.tf
+++ 04-Terraform/03-Ansible-Deploy/local_exec.tf
@@ -6,12 +6,12 @@ terraform {
 }
 
 provider "aws" {
-  region = "us-east-2"
+  region = "us-west-2"
   
 }
 
 resource "aws_instance" "backend" {
-  ami                    = "ami-0d5d9d301c853a04a"
+  ami                    = "ami-06d51e91cea0dac8d"
   instance_type          = "t2.micro"
   key_name               = "${var.key_name}"
   vpc_security_group_ids = ["${var.sg-id}"]
